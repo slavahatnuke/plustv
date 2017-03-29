d6772f4113cb80e1e527881ec0cd22dc0a0c31f5
slava hatnuke <slava.hatnuke@gmail.com>
2017-03-29 05:13:28 +0300

    wip

---
+++ client/.scas.js
@@ -0,0 +1,9 @@
+module.exports = {
+    title: '$ scas [actions] <arguments> <options>', // you can rewrite this for you goals
+    description: 'Scaffolding console', // you can rewrite this for you goals
+    imports: [
+        // it needs to support default help
+        { path: 'node_modules/scas-angular/scas/angular' },
+        { path: 'scas/.scas' }
+    ]
+}
